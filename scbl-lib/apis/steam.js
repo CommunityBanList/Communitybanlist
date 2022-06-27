424396b4a769b30779039da524c694d92191133d
Michael-Phoenix <3187643-Michael-Phoenix@users.noreply.gitlab.com>
2022-06-27 23:37:52 +0200

    Changed to return proper error.message for BM and steam

--- scbl-lib/apis/steam.js
+++ scbl-lib/apis/steam.js
@@ -5,6 +5,12 @@ import { STEAM_API_KEY } from '../config.js';
 if (!STEAM_API_KEY) throw new Error('Environmental variable STEAM_API_KEY must be provided.');
 
 export default async function (method, url, params, data = {}) {
+  axios.interceptors.response.use(function (response) {
+    return response;
+  }, function (error) {
+    //add proper Error reporting here, maybe sentry so that the error does at least show up somewhere
+    return Promise.reject(error.message);
+  });
   return axios({
     method,
     url: 'http://api.steampowered.com/' + url,
