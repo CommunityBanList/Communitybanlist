41f954e9c48f9faa08146fde1b534018a51e4a48
RobotoLev <robotolev@yandex.ru>
2023-07-25 11:32:05 +0300

    Add ru regexps to classify-ban-reason.js

--- scbl-lib/utils/classify-ban-reason.js
+++ scbl-lib/utils/classify-ban-reason.js
@@ -1,43 +1,43 @@
 const reasons = {
-  Cheating: /cheat|hack|作弊|\bhile|trich|betr(ü|u)/i,
-  Glitching: /glitch/i,
-  Exploiting: /exploit|剝削|istismar|ausnutz/i,
+  Cheating: /cheat|hack|作弊|\bhile|trich|betr(ü|u)|чит|хак/i,
+  Glitching: /glitch|глитч|багоюз/i,
+  Exploiting: /exploit|剝削|istismar|ausnutz|эксплойт/i,
 
-  Teamkilling: /team ?kill|\btk|ITK|[0-9]x?tk|int?tk|team(be|ab)schuss/i,
+  Teamkilling: /team ?kill|\btk|ITK|[0-9]x?tk|int?tk|team(be|ab)schuss|тк|тим ?килл/i,
 
-  Trolling: /troll/i,
-  Griefing: /gr(ie|ei)f/i,
+  Trolling: /troll|тролл/i,
+  Griefing: /gr(ie|ei)f|гриф(ер|инг)/i,
   'Wasting Assets':
-    /destroying assets|wast(e|ing)|taxi|israf|(friendly|dost) (asset|fob|hab)|sabotage|asset destruct|zerstör|verschwend|eigene(s)? (radio|fob|vehikel|fahrzeug|hab|hub)/i,
+    /destroying assets|wast(e|ing)|taxi|israf|(friendly|dost) (asset|fob|hab)|sabotage|asset destruct|zerstör|verschwend|eigene(s)? (radio|fob|vehikel|fahrzeug|hab|hub)|слив|руин/i,
   Ghosting: /ghosting/i,
 
   Toxic:
-    /disrespect|flam(e|ing)|har(r)?as(s)?|insult|language|offensive|rude|toxic|sayg(i|ı)s(i|ı)z|hakaret|irrespect|respektlos|beleidig(ung|en)/i,
+    /disrespect|flam(e|ing)|har(r)?as(s)?|insult|language|offensive|rude|toxic|sayg(i|ı)s(i|ı)z|hakaret|irrespect|respektlos|beleidig(ung|en)|оскорблени(е|я)/i,
   'Abusive Language/Hate Speech':
     /abusive|bigot|derogatory|discriminat|hate ?spe(e|a)ch|(homo|trans)(-| )?phobi|nazi|racial|rac(si|is)m|racist|sexism|sexist|(küfür|kufur)|mal(é|e)diction|種族主義|fluchen|semitism|nigg(a|er)|fag|slur|swastika|schimpfw(o|ö)rt|rassis(t|m)|missbr(a|ä)uch|(f|n)(-| )?(word|bomb)/i,
 
-  AFK: /afk|unassigned/i,
+  AFK: /afk|unassigned|афк/i,
   'Breaking Seeding Rules': /seed/i,
   'Breaking Vehicle Priority Rules':
-    /priority|(ara(ç|c) (kural|ihlali))|(vic|vehicle|asset) (name )?claim|claim rule|ignor(ing|ed) claim/i,
-  Camping: /camping|min(e|ing)/i,
+    /priority|(ara(ç|c) (kural|ihlali))|(vic|vehicle|asset) (name )?claim|claim rule|ignor(ing|ed) claim|уго?н|клейм/i,
+  Camping: /camping|min(e|ing)|кемп/i,
   'Current or Recent VAC Ban': /vac ban/i,
-  'Discussing politics': /politi(c|k|s)|siyaset/i,
+  'Discussing politics': /politi(c|k|s)|siyaset|полит/i,
   'Helicopter Ramming': /(ge)?ramm(ing|en|t)/i,
   Hindering: /(be|ver)?hinder(ing|ed|t|ung)/i,
-  Impersonation: /impersonat|ausge(ge)?ben/i,
+  Impersonation: /impersonat|ausge(ge)?ben|имперсон/i,
   'Locked Squad': /(locked|geschlossenes) squad/i,
-  'No SL Kit': /sl kit/i,
+  'No SL Kit': /sl kit|сквадной без кита|кита? сквадного/i,
   Recruiting: /recruiting|rekrutier/i,
-  'Soloing Vehicles': /crewman|manning|solo|(1|one)(-| )?man/i,
-  Spamming: /spam/i,
+  'Soloing Vehicles': /crewman|manning|solo|(1|one)(-| )?man|соло(-| )?тех/i,
+  Spamming: /spam|спам/i,
   'Squad Baiting':
-    /bai(t|ting)|creat(?:ed?|ing)(?: a)? squad|pass sl|lead it|(of|to) lead|pas(s|sing)( |-)?SL/i,
+    /bai(t|ting)|creat(?:ed?|ing)(?: a)? squad|pass sl|lead it|(of|to) lead|pas(s|sing)( |-)?SL|перекид/i,
   'Stealing Assets': /steal|çalma|(ge)?st(e|o)hlen/i,
-  Streamsniping: /streamsniping|yayinc(i|ı)/i,
+  Streamsniping: /streamsniping|yayinc(i|ı)|стрим ?снайп/i,
   'Sharing team info':
-    /shar(e|ing) (ticket|team|hab|fob|info|game|enemy)|ticket (count|sharing)|(info|intel) shar(e|ing)/i,
-  Advertising: /advertising|werb(ung|en)/i,
+    /shar(e|ing) (ticket|team|hab|fob|info|game|enemy)|ticket (count|sharing)|(info|intel) shar(e|ing)|слив инф/i,
+  Advertising: /advertising|werb(ung|en)|реклам/i,
   Ableism: /ableis(t|m)/i
 };
 
